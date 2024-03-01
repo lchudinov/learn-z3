2f1deae677cfa15fffb9f01c74d1f6f3900a81a9
Leonty Chudinov <leonty.chudinov@gmail.com>
2024-03-01 08:24:45 +0500

    More done

--- python/logical_interfaces_to_z3.py
+++ python/logical_interfaces_to_z3.py
@@ -7,3 +7,9 @@ s.add(Or(Tie, Shirt),
       Or(Not(Tie), Not(Shirt)))
 print (s.check())
 print (s.model())
+
+Z = IntSort()
+f = Function('f', Z, Z)
+x, y, z = Ints('x y z')
+A = Array('A', Z, Z)
+fml = Implies(x + 2 == y, f(Store(A, x, 3)[y - 2]) == f(y - x + 1))
